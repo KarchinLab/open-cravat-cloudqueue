97465ac73d6dcbe09d12bb9e7455c97b4c96a888
kmoad <kyle.moad@gmail.com>
2020-12-01 04:01:25 -0500

    init authorized-users document

--- initdb.py
+++ initdb.py
@@ -1,16 +1,27 @@
 from google.cloud import firestore
+import yaml
 
 db = firestore.Client()
-is_ref = db.collection(u'environment').document(u'imageStatus')
-is_content = {u'imageStatus' : "Not Created"}
+
+is_ref = db.collection('environment').document('imageStatus')
+is_content = {'imageStatus' : "Not Created"}
 if is_ref.get().exists:
     is_ref.update(is_content)
 else:
     is_ref.create(is_content)
 
-anno_ref = db.collection(u'environment').document(u'annotators')
-anno_content = {u'annotators' : ""}
+anno_ref = db.collection('environment').document('annotators')
+anno_content = {'annotators' : ""}
 if anno_ref.get().exists:
     anno_ref.update(anno_content)
 else:
     anno_ref.create(anno_content)
+
+config = yaml.safe_load(open('config.yml'))
+users_ref = db.collection('environment').document('authorized-users')
+users_content = {'authorizedUsers':config['users']}
+print(users_content)
+if users_ref.get().exists:
+    anno_ref.update(users_content)
+else:
+    users_ref.create(users_content)
