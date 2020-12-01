e5600a2d2454cb8b3059922da4a78e9245c1b086
kmoad <kyle.moad@gmail.com>
2020-12-01 02:32:08 -0500

    add emal/pw auth to deploy script

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -xe
 
 # Read in project-wide environment variables
 python3 makeenv.py config.yml
@@ -23,36 +23,53 @@ echo 'Enabling cloudfunction and cloudbuild APIs'
 gcloud services enable cloudfunctions.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 
+# Firebase
+echo "This tool uses Firebase to provide authentication, database management, and cloud storage."
+echo "Unfortunately, many Firebase services cannot be initialized without user input."
+echo "You will be directed to enable some Firebase services in a browser, and to reply to some command line prompts."
+read -p "Press enter to continue"
 rm -f .firebaserc
 #TODO add firebase only if not already added
-# firebase projects:addfirebase $GCP_PROJECT
+firebase projects:addfirebase $GCP_PROJECT
 firebase use $GCP_PROJECT
-echo "Deploying jobs database"
-echo "Open https://console.firebase.google.com/project/$GCP_PROJECT/firestore and create a firestore database with the default settings"
-read -p "Press enter when finished"
 
-echo "Accept the default paths for firestore.rules and firestore.indexes.json. Do not overwrite the files."
-read -p "Press enter when finished"
+# Authentication
+echo "Open the link below and enable Email/Password authentication. Do not enable Email link authentication"
+echo "https://console.firebase.google.com/u/0/project/$GCP_PROJECT/authentication/providers"
+read -p "Press enter to continue"
+
+# Firestore
+echo "Open the link below and create a firestore database with the default settings"
+echo "https://console.firebase.google.com/project/$GCP_PROJECT/firestore"
+read -p "Press enter to continue"
+echo "Deploying firestore database configuration"
+echo "Follow the prmpts to accept the default paths for firestore.rules and firestore.indexes.json. Do not overwrite the files."
+read -p "Press enter to continue"
 firebase init firestore
-echo "Deploying job output storage"
-echo "Accept the default path for storage.rules"
-firebase init storage
-# firebase deploy
 echo "Deploy initial data to firestore"
 #TODO this is hanging
-# python3 initdb.py
+python3 initdb.py
 
-# OCQ_SERVICE_ACCOUNT_EMAIL="$OCQ_SERVICE_ACCOUNT_NAME@$GCP_PROJECT.iam.gserviceaccount.com"
+# Cloud storage
+echo "Deploying cloud storage configuration"
+echo "Follow the prompts to accept the default path for storage.rules"
+firebase init storage
+firebase deploy
+
+# Service account
+OCQ_SERVICE_ACCOUNT_EMAIL="$OCQ_SERVICE_ACCOUNT_NAME@$GCP_PROJECT.iam.gserviceaccount.com"
 echo "Creating service account $OCQ_SERVICE_ACCOUNT_NAME with editor role"
 gcloud iam service-accounts create "$OCQ_SERVICE_ACCOUNT_NAME" --description "$OCQ_SERVICE_ACCOUNT_DESC" --display-name "$OCQ_SERVICE_ACCOUNT_DISPLAY"
 gcloud projects add-iam-policy-binding $GCP_PROJECT --member "serviceAccount:$OCQ_SERVICE_ACCOUNT_EMAIL" --role 'roles/editor'
 
+# PubSub
 echo "Deploying pubsub message queues"
 gcloud pubsub topics create $OCQ_JOB_START_TOPIC
 gcloud pubsub subscriptions create $OCQ_JOB_START_SUB --topic $OCQ_JOB_START_TOPIC --ack-deadline 60
 gcloud pubsub topics create $OCQ_JOB_DONE_TOPIC
 gcloud pubsub subscriptions create $OCQ_JOB_DONE_SUB --topic $OCQ_JOB_DONE_TOPIC --ack-deadline 60
 
+# Cloud functions
 #TODO add region to functions (probably to everything else too)
 echo 'Deploying functions'
 gcloud beta functions deploy $OCQ_INSTANCE_CREATE_FUNC \
