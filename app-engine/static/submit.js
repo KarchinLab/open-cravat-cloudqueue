9c3c68d849afc909390264014148c80498160b74
kmoad <kyle.moad@gmail.com>
2021-05-12 13:08:40 -0400

    download popover, db and csv

--- app-engine/static/submit.js
+++ app-engine/static/submit.js
@@ -8,6 +8,8 @@ async function main() {
     submitBtn.addEventListener('click',()=>{submit()})
 };
 
+const jobs = {};
+
 async function loadAnnotators () {
     const instanceRef = firebase.firestore()
         .collection('environment')
@@ -55,6 +57,7 @@ function loadJobs () {
 }
 
 function addJob(jobId, job) {
+    jobs[jobId] = job;
     let tbody = $('#jobstable tbody');
     let tr = $(document.createElement('tr'));
     fillJobRow(tr, jobId, job);
@@ -76,13 +79,13 @@ function fillJobRow(tr, jobId, job) {
     // Output
     let outputTd = $(document.createElement('td'))
     if (job.status.code >= 40) {
-        let dbLink = $(document.createElement('a'))
-            .text('Database')
-            .attr('target','_blank')
-        outputTd.append(dbLink);
-        firebase.storage().ref().child(job.output).getDownloadURL().then((url)=>{
-            dbLink.attr('href',url);
-        })
+        let resultsBtn = $(document.createElement('button'))
+            .text('Results')
+            .addClass('btn')
+            .addClass('btn-outline-dark')
+            .addClass('job-results-btn')
+            .click(showDownloadPopover);
+        outputTd.append(resultsBtn);
     } else {
         outputTd.text('Not yet');
     }
@@ -129,9 +132,11 @@ function deleteJobHandler(event) {
 
 function removeJob(jobId) {
     $(`tr[jobid=${jobId}]`).remove();
+    delete jobs[jobId];
 }
 
 function modifyJob(jobId, job) {
+    jobs[jobId] = job;
     let tr = $(`tr[jobid=${jobId}]`);
     if (tr) {
         tr.empty();
@@ -183,6 +188,74 @@ async function submit() {
     })
 }
 
+function showDownloadPopover(event) {
+    const rect = event.target.getBoundingClientRect();
+    const jobId = $(event.target).parents('tr').attr('jobid');
+    const job = jobs[jobId];
+    let dbPath = null;
+    let csvPath = null;
+    if (job.hasOwnProperty('output') && job.output != null) {
+        if (typeof(job.output) === 'object') {
+            dbPath = job.output.database ? job.output.database : null;
+            csvPath = job.output.csv ? job.output.database : null;
+        } else if (typeof(job.output) === 'string') {
+            dbPath = job.output;
+            csvPath = null;
+        }
+    }
+    const popover = $('#download-popover');
+    if (dbPath) {
+        popover.find('#database-download')
+            .prop('disabled',false)
+            .click(()=>{
+                firebase.storage().ref().child(job.output.database).getDownloadURL().then((url)=>{
+                    downloadURL(url);
+                });
+            });
+    } else {
+        popover.find('#database-download').prop('disabled',true);
+    }
+    if (csvPath) {
+        popover.find('#csv-download')
+            .prop('disabled',false)
+            .click(()=>{
+                firebase.storage().ref().child(csvPath).getDownloadURL().then((url)=>{
+                    downloadURL(url);
+                });
+        });
+    } else {
+        popover.find('#csv-download').prop('disabled',true);
+    }
+    
+    // Keep at bottom
+    popover.css('display','block')
+        .css('top',`${rect.bottom+5}px`)
+        .css('left',`${rect.left}px`);
+}
+
+function hideDownloadPopover() {
+    $('#download-popover').css('display','none');
+}
+
+$(document).on('click',e=>{
+    if ($(e.target).closest($('#download-popover')).length === 0 && ! e.target.classList.contains('job-results-btn')) {
+        hideDownloadPopover();
+    }
+})
+
+function downloadURL(url, fileName) {
+    const a = document.createElement("a");
+    a.href = url;
+    a.target = '_blank';
+    fileName = fileName ? fileName : url.split("/").pop();
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+}
+
+// Keep at bottom
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         main();
